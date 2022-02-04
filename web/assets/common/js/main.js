4ff3b1920038d040e6dfd8dd91c4de5be60f1e8c
wewawe <william0990@yahoo.com>
2022-02-04 16:06:57 +0700

    rev 12

--- web/assets/common/js/main.js
+++ web/assets/common/js/main.js
@@ -1,13 +1,15 @@
+/* Navbar transparent */
 var nav = document.querySelector("nav");
 
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 50) {
     nav.classList.add("bg-dark", "shadow");
   } else {
     nav.classList.remove("bg-dark", "shadow");
   }
 });
 
+/* AOS Animation*/
 AOS.init({
   easing: "ease-out-back",
   duration: 1000,
